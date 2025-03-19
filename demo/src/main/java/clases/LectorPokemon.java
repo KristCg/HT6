b4ce22781680b626b04eef64d1f198c5d0381ed5
KristCg <kriscastillo515@gmail.com>
2025-03-19 10:32:35 -0600

    Creacion de la clase LectorPokemon

---
+++ demo/src/main/java/clases/LectorPokemon.java
@@ -0,0 +1,34 @@
+package clases;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LectorPokemon {
+
+    public static final String RUTA_ARCHIVO = "C:\\Users\\Admin\\Documents\\Yio\\U\\HT6\\demo\\src\\main\\java\\clases\\pokemon_data_pokeapi.csv";
+    public static Map<String, Pokemon> readPokemons() throws IOException {
+        Map<String, Pokemon> pokemonMap = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(RUTA_ARCHIVO))) {
+            String line;
+            boolean isFirstLine = true;
+
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] columns = line.split(",");
+                Pokemon pokemon = new Pokemon(columns);
+                pokemonMap.put(pokemon.getName(), pokemon);
+            }
+        }
+
+        return pokemonMap;
+    }
+}
