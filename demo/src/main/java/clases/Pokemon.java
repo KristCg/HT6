3f6324a605776a6beb1875098b030dbef79a4c28
KristCg <kriscastillo515@gmail.com>
2025-03-19 10:34:17 -0600

    Clase Pokemon

---
+++ demo/src/main/java/clases/Pokemon.java
@@ -0,0 +1,48 @@
+package clases;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pokemon {
+    private String name;
+    private String type1;
+    private String type2;
+    private List<String> abilities;
+
+
+    public Pokemon(String[] columns) {
+        this.name = columns[0];
+        this.type1 = columns[2];
+        this.type2 = columns[3];
+        this.abilities = Arrays.asList(columns[7].split(";"));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public List<String> getAbilities() {
+        return abilities;
+    }
+
+    @Override
+    public String toString() {
+
+        String abilitiesStr = String.join(", ", abilities);
+
+        return "Pokemon{" +
+                "nombre='" + name + '\'' +
+                ", tipo1='" + type1 + '\'' +
+                ", tipo2='" + type2 + '\'' +
+                ", habilidades=" + abilitiesStr +
+                '}';
+    }
+}
