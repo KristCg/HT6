31862c9dd26fbebb53b097b10a4444e866d62069
KristCg <kriscastillo515@gmail.com>
2025-03-19 10:33:38 -0600

    Clase Factory

---
+++ demo/src/main/java/clases/MapFactory.java
@@ -0,0 +1,21 @@
+package clases;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
+public class MapFactory {
+    public static Map<String, Pokemon> createMap(int choice) {
+        switch (choice) {
+            case 1:
+                return new HashMap<>();
+            case 2:
+                return new TreeMap<>();
+            case 3:
+                return new LinkedHashMap<>();
+            default:
+                throw new IllegalArgumentException("Opcion Invalida");
+        }
+    }
+}
